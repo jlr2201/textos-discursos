0a300c1592a40959730e7e3b2e03ea0fb48e97eb
Junior Rivas <jlr2201@gmail.com>
2023-04-22 22:01:42 -0500

    Inicio de una nueva version, cuando se complete sera la 2.0

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const textos = [
+    "<p>Lucas 22:19, 20<br>Después tomó un pan, le dio gracias a Dios, lo partió, se lo dio a ellos y les dijo: “Esto representa mi cuerpo, que será dado en beneficio de ustedes. Sigan haciendo esto en memoria de mí”...<p>",
+    "<p>También, después de haber cenado, hizo lo mismo con la copa. Les dijo: Esta copa representa el nuevo pacto, validado con mi sangre, que va a ser derramada en beneficio de ustedes<p>",
+    "<p>Romanos 5:12<br>Así pues, por medio de un solo hombre, el pecado entró en el mundo y por medio del pecado entró la muerte, y así fue como la muerte se extendió a todos los hombres, porque todos habían pecado...</p>",
+    "<p>Juan 3:16<br>Porque Dios amó tanto al mundo que entregó a su Hijo unigénito para que nadie que demuestre tener fe en él sea destruido, sino que tenga vida eterna.</p>",
+    "<p>Romanos 5:19<br>Porque, tal como muchos llegaron a ser pecadores por la desobediencia de un solo hombre, del mismo modo muchos llegarán a ser justos por la obediencia de una sola persona.</p>",
+    "<p>Hebreos 2:9<br>Ya vemos a Jesús —que fue hecho un poco inferior a los ángeles— coronado de gloria y honra por haber sufrido hasta el punto de morir, para que por la bondad inmerecida de Dios él probara la muerte por todos.</p>",
+    "<p>Apocalipsis 14:1<br>Entonces vi al Cordero de pie en el monte Sion, y con él 144.000 que llevan escritos en la frente el nombre de él y el nombre de su Padre.</p>",
+    "<p>En ese tiempo, los ojos de los ciegos serán abiertos y los oídos de los sordos serán destapados...</p>",
+    "<p> Isaias 35:5, 6<br>En ese tiempo, el cojo trepará como un ciervo y la lengua del mudo gritará de alegría. Pues brotarán aguas en el desierto y torrentes en la llanura desértica.</p>",
+    "<p>Ahora bien, ustedes son los que en mis pruebas se han mantenido a mi lado. Y yo hago un pacto con ustedes para un reino, así como mi Padre ha hecho un pacto conmigo...</p>",
+    "<p>Lucas 22:28-30<br>para que coman y beban a mi mesa en mi Reino y se sienten en tronos para juzgar a las 12 tribus de Israel.Lucas 22:28-30</p>",
+    "<p>1 Corintios 11:26<br>Porque, cada vez que coman de este pan y beban de esta copa, estarán proclamando la muerte del Señor, hasta que él venga.</p>",
+    "<p>Porque yo les transmití lo mismo que recibí del Señor: que el Señor Jesús, la noche en que iba a ser traicionado, tomó un pan...</p>",
+    "<p>1 Corintios 11:23, 24<br>y, después de darle gracias a Dios, lo partió y dijo: “Esto representa mi cuerpo, que es dado en beneficio de ustedes. Sigan haciendo esto en memoria de mí”.</p>",
+    "<p>1 Corintios 11:25<br>Después de haber cenado, también hizo lo mismo con la copa y dijo: “Esta copa representa el nuevo pacto, validado con mi sangre. Sigan haciendo esto en memoria de mí cada vez que beban de ella”.</p>",
+    "<p>El amor del Cristo nos obliga, pues esta es la conclusión a la que hemos llegado: que un hombre murió por todos; de modo que todos ya habían muerto...</p>",
+    "<p>Corintios 5:14, 15<br>Y él murió por todos para que los que viven no vivan ya para sí mismos, sino para el que murió por ellos y fue resucitado.</p>",
+    "<p>2 Corintios 5:14, 15<br>Y él murió por todos para que los que viven no vivan ya para sí mismos, sino para el que murió por ellos y fue resucitado.</p>"
+]
